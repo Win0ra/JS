b8261ea51db26595cb9aba983c2220334988cd6d
Win0ra <maevabouvard@gmail.com>
2024-05-14 14:09:45 +0200

    Promise

--- 10_promise.js
+++ 10_promise.js
@@ -6,7 +6,12 @@
  */
 
 const sleep = () => {
-
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, 2000);
+    });
 };
+
 
 module.exports = {sleep};
